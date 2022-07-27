418e12ede29ed6e665ed1e488c6f41d7a86718fe
yusufneeson <yusufneeson@gmail.com>
2022-07-27 09:08:44 +0700

    Account Model: set $fillable

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'icon'
+    ];
 }
