e662b218f1931b25abcc5d2b983b05ec1b6609f4
yusufneeson <yusufneeson@gmail.com>
2022-07-27 09:09:30 +0700

    Convert Model: set $fillable

--- app/Models/Convert.php
+++ app/Models/Convert.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Convert extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'transaction_id',
+        'dest_id'
+    ];
 }
